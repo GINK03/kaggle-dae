4b2745d5e0ca352cadbb7245ddd0eddd9673e978
Your Name <gim.kobayashi@gmail.com>
2018-07-04 11:35:50 +0900

    update

--- michael/dae.py
+++ michael/dae.py
@@ -16,24 +16,40 @@ from keras.layers.merge         import Concatenate as Concat
 from keras.layers.noise         import GaussianNoise as GN
 from keras.layers.merge         import Dot,Multiply
 from keras import backend as K
+import tensorflow as tf
+from tensorflow.python import debug as tf_debug
+from tensorflow.python.debug.lib.debug_data import has_inf_or_nan
 
 
-input   = Input(shape=(784,))
-encoded = Dense(128, activation='relu')(input_img)
-encoded = Dense(64, activation='relu')(encoded)
-encoded = Dense(32, activation='linear')(encoded)
+input   = Input(shape=(229,))
+encoded = Dense(229, activation='linear')(input)
+#encoded = Dense(64, activation='relu')(encoded)
+#encoded = Dense(32, activation='relu')(encoded)
+#encoded = Dense(16, activation='relu')(encoded)
 
-decoded = Dense(64, activation='relu')(encoded)
-decoded = Dense(128, activation='relu')(decoded)
-decoded = Dense(784, activation='linear')(decoded)
+#decoded = Dense(32, activation='relu')(encoded)
+#decoded = Dense(64, activation='relu')(decoded)
+#decoded = Dense(128, activation='linear')(encoded)
+decoded = Dense(229, activation='linear')(encoded)
 
-autoencoder = Model(input_img, decoded)
-autoencoder.compile(optimizer='sgd', loss='mse')
+dae = Model(input, decoded)
+dae.compile(optimizer='adam', loss='mae')
 
-'''
-autoencoder.fit(x_train, x_train,
+def set_debugger_session():
+    sess = K.get_session()
+    sess = tf_debug.LocalCLIDebugWrapperSession(sess)
+    sess.add_tensor_filter('has_inf_or_nan', has_inf_or_nan)
+    K.set_session(sess)
+#set_debugger_session()
+import pandas as pd
+
+tdf = pd.read_csv('vars/one_hot_train.csv')
+Tdf = pd.read_csv('vars/one_hot_test.csv')
+df = pd.concat([tdf, Tdf], axis=0)
+
+dae.fit(df.values, df.copy().values,
                 epochs=100,
                 batch_size=256,
-                shuffle=True,
-                validation_data=(x_test, x_test))
-'''
+                shuffle=True,)
+                #validation_data=(x_test, x_test))
+
