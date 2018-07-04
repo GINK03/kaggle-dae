ac792105e216ac5385e6170ecf10328a1404945c
Your Name <gim.kobayashi@gmail.com>
2018-07-04 16:04:09 +0900

    update

--- michael/swap_noise.py
+++ michael/swap_noise.py
@@ -2,19 +2,24 @@
 
 import numpy as np
 import random
+from numba.decorators import jit
+
+@jit
 def noise(array):
   print('now noising') 
   height = len(array)
   width = len(array[0])
-
-  copy = np.copy(array)
+  print('start rand')  
+  rands = np.random.uniform(0, 1, (height, width) )
+  print('finish rand')  
+  copy  = np.copy(array)
 
   for h in range(height):
     for w in range(width):
-      if random.random() <= 0.15:
+      if rands[h, w] <= 0.15:
         swap_target_h = random.randint(0,h)
         copy[h, w] = array[swap_target_h, w]
-
+      #print(rands[h,w])
   print('finish noising') 
   return copy
   
